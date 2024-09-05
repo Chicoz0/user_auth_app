d2536a36a50e9fa417845e701c8fdaf6f49dd73d
Francisco P. Lemos <franciscodepaulalemosjogos@gmail.com>
2024-09-05 10:39:41 -0300

    Logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,26 @@
 Rails.application.routes.draw do
   
-  get "/about", to: "about#index"
- # root "main#index"   OR    root to: "main#index"    OR get "", to: "main#index"
- # get "/", to: "main#index"
+ # Root alternatives: root "main#index"                  root to: "main#index"                get "", to: "main#index"                 get "/", to: "main#index"
   root "main#index"
 
-  #rout para registration
+  #Rout for user actions
   get"sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #Rout para página about
+  get "/about", to: "about#index"
 
+  # Routes that already were in here from step one
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
 
 
   
